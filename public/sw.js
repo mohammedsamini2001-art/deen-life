2ac939e58f50f34810911f34de52ad7e40387299
Your Name <you@example.com>
2026-09-15 13:45:05 +0300

    fix: align service worker with PWA icons

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,8 @@ const APP_SHELL = [
   '/',
   '/index.html',
   '/manifest.webmanifest',
-  '/pwa-icon.svg'
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ]
 
 self.addEventListener('install', event => {
