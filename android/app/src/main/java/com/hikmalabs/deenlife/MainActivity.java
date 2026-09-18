9d71145a296ca63f9235814835bd096cd01a57cc
Your Name <you@example.com>
2026-09-18 09:32:59 +0300

    chore: checkpoint capacitor android setup

---
+++ android/app/src/main/java/com/hikmalabs/deenlife/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hikmalabs.deenlife;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hikmalabs.deenlife',
+  appName: 'DEEN LIFE',
+  webDir: 'dist'
+};
+
+export default config;
